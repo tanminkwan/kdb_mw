dd844407be886d7787ebd50338916abd76348602
tanminkwan <tanminkwan@gmail.com>
2026-02-12 08:51:53 +0900

    사용하지 않는 auto_report 폴더 삭제

--- app/auto_report/testMain.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from testGetWasStatus import getAccessToken, getWasStatus, getNotRunningWasList
-from testXlsx import createDailyCheckXlsx
-from testSelenium import getTasks
-from testHwp2 import recreateHwp
-from testSmtp import send_kdbMail
-from auto_report import run_auto_report
-
-run_auto_report()
