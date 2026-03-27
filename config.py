86ce80cb590ba5981915dc4d0d227f699518de9a
tanminkwan <tanminkwan@gmail.com>
2026-03-27 10:46:28 +0900

    chore: update version to 20260327.001

--- config.py
+++ config.py
@@ -82,7 +82,7 @@ CSRF_ENABLED = True
 # GLOBALS FOR APP Builder
 # ------------------------------
 # Uncomment to setup Your App name
-APP_NAME = "리발소(VER:20260326.009)"
+APP_NAME = "리발소(VER:20260327.001)"
 
 # Uncomment to setup Setup an App icon
 # APP_ICON = "static/img/logo.jpg"
